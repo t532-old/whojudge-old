2abf3c5f94bcdcd25893e8786862cab60ff03738
t532 <2037246484@qq.com>
2019-05-02 17:32:46 +0800

    modify error classes   + change all status codes in error classes to getters   + add error classes those may needed in the future

--- src/util/error.ts
+++ src/util/error.ts
@@ -1,42 +1,56 @@
 export class UsernameNotExistError extends Error {
-    status = 404
+    get status() { return 404 }
     constructor(username: string) {
         super(`user "${username}" does not exist`)
     }
 }
 export class UsernameExistsError extends Error {
-    status = 403
+    get status() { return 403 }
     constructor(username: string) {
         super(`user "${username}" does not exist`)
     }
 }
 export class UsernameIllegalError extends Error {
-    status = 403
+    get status() { return 403 }
 }
 export class PasswordIncorrectError extends Error {
-    status = 403
+    get status() { return 403 }
     constructor() {
         super(`wrong password`)
     }
 }
 export class TokenNotExistError extends Error {
-    status = 404
+    get status() { return 403 }
     constructor() {
         super(`token does not exist`)
     }
 }
 export class ProblemNotExistError extends Error {
-    status = 404
+    get status() { return 404 }
     constructor(id: number) {
         super(`problem ${id} does not exist`)
     }
 }
 export class TestcaseNotExistError extends Error {
-    status = 404
+    get status() { return 404 }
     constructor(id: number) {
         super(`testcase ${id} does not exist`)
     }
 }
 export class PermissionDeniedError extends Error {
-    status = 403
+    get status() { return 403 }
+}
+
+export class SubmissionNotExistError extends Error {
+    get status() { return 404 }
+    constructor(id: number) {
+        super(`submission ${id} does not exist`)
+    }
+}
+
+export class InternalCallOnlyError extends Error {
+    get status() { return 403 }
+    constructor() {
+        super(`this api is intended to call internally only`)
+    }
 }
